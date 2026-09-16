5c6fa42ea57cee0b310afc77e6380754e0088f17
mth310-sys <mth.310@gmail.com>
2026-09-16 12:54:40 +0900

    Make PachislotBay a self-contained reusable item

--- pachirou/scripts/pachislot_bay_item.gd
+++ pachirou/scripts/pachislot_bay_item.gd
@@ -3,31 +3,29 @@ extends Node2D
 
 enum Direction {
 	LEFT_DOWN,
-	LEFT_UP,
-	RIGHT_UP,
-	RIGHT_DOWN,
 }
 
 var direction: Direction = Direction.LEFT_DOWN
 var renderer: Callable
 
-func setup(p_direction: Direction, p_renderer: Callable) -> void:
-	direction = p_direction
+@onready var frame: Node2D = $Frame
+@onready var equipment: Node2D = $Equipment
+@onready var stool: Node2D = $Stool
+
+func setup(p_renderer: Callable) -> void:
 	renderer = p_renderer
-	name = "PachislotBay_%s" % direction_name()
+	name = "PachislotBay"
 
 func build() -> void:
+	clear_visuals()
 	if renderer.is_valid():
-		renderer.call(self, direction)
+		renderer.call(self)
 
-func direction_name() -> String:
-	match direction:
-		Direction.LEFT_DOWN:
-			return "LEFT_DOWN"
-		Direction.LEFT_UP:
-			return "LEFT_UP"
-		Direction.RIGHT_UP:
-			return "RIGHT_UP"
-		Direction.RIGHT_DOWN:
-			return "RIGHT_DOWN"
-	return "UNKNOWN"
+func clear_visuals() -> void:
+	_clear_children(frame)
+	_clear_children(equipment)
+	_clear_children(stool)
+
+func _clear_children(parent: Node2D) -> void:
+	for child in parent.get_children():
+		child.queue_free()

c14c41397b5a4a8257c0cf9e027d1df3979a2d4d
mth310-sys <mth.310@gmail.com>
2026-09-18 01:14:53 +0900

    Frame humanoid correctly in Character Lab camera

--- pachirou/tests/character_lab.gd
+++ pachirou/tests/character_lab.gd
@@ -5,3 +5,8 @@ func _ready() -> void:
 	var humanoid := humanoid_scene.instantiate() as Node3D
 	humanoid.name = "Male20s_175cm"
 	add_child(humanoid)
+
+	var camera := get_node("Camera3D") as Camera3D
+	camera.position = Vector3(2.8,1.65,3.8)
+	camera.look_at(Vector3(0.0,0.88,0.0),Vector3.UP)
+	camera.size = 2.2
